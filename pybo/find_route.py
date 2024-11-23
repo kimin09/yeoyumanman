5b246a5f76139d1ee54db3a44fcf1e72bf21cfce
kimkimin <kimin090304@gmail.com>
2024-11-23 13:39:12 +0900

    aefarfhgedrfaarfaeeh

--- pybo/find_route.py
+++ pybo/find_route.py
@@ -3,7 +3,7 @@ import csv
 
 spent_time = {}
 
-f = open("spent_time.csv", "r")
+f = open("spent_time.csv", "r", encoding="euc-kr")
 reader = csv.reader(f)
 
 for row in list(reader):
@@ -24,7 +24,7 @@ for row in list(reader):
 
 congestion = {}
 
-f = open("congestion_by_station.csv", "r")
+f = open("congestion_by_station.csv", "r", encoding="euc-kr")
 reader = csv.DictReader(f)
 times = ['5시30분', '6시00분', '6시30분', '7시00분', '7시30분', '8시00분', '8시30분', '9시00분', '9시30분', '10시00분', '10시30분', '11시00분', '11시30분', '12시00분', '12시30분', '13시00분', '13시30분', '14시00분', '14시30분', '15시00분', '15시30분', '16시00분', '16시30분', '17시00분', '17시30분', '18시00분', '18시30분', '19시00분', '19시30분', '20시00분', '20시30분', '21시00분', '21시30분', '22시00분', '22시30분', '23시00분', '23시30분', '00시00분', '00시30분']
 for row in list(reader):
