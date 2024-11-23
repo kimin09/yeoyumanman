bf1c0074b0d22ea385c5ecb29fabc061da522243
kimkimin <kimin090304@gmail.com>
2024-11-23 14:17:44 +0900

    sdfsfs

--- pybo/forms.py
+++ pybo/forms.py
@@ -4,5 +4,5 @@ from wtforms.validators import DataRequired
 
 class RouteForm(FlaskForm):
     # 비어있지 않게 입력할 수 있는 출발역 필드 만드는 코드입니다.
-    start = TextAreaField('내용', validators=[DataRequired('시작 역은 필수입력 항목입니다.')])
-    end = TextAreaField('내용', validators=[DataRequired('도착 역은 필수입력 항목입니다.')])
+    start = TextAreaField('출발역', validators=[DataRequired('출발 역은 필수입력 항목입니다.')])
+    end = TextAreaField('도착역', validators=[DataRequired('도착 역은 필수입력 항목입니다.')])
